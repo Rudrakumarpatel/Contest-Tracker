f356915a763c27bc47dff415aafdf98abb946633
Rudrakumar Patel <rudrapatel2992003@gmail.com>
2025-03-20 08:18:00 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ContestProvider } from "./context/ContestContext";
 import Home from "./pages/Home";
-// import Bookmarks from "./pages/Bookmarks";
-import AdminPanel from "./pages/AdminPanel";
 import Navbar from "./components/Navbar";
 import ThemeToggle from "./components/ThemeToggle";
 import React from "react";
@@ -14,7 +12,6 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/admin" element={<AdminPanel />} />
         </Routes>
       </Router>
     </ContestProvider>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,6 @@ const Navbar = () => {
       <h1 className="text-lg font-bold">Contest Tracker</h1>
       <div>
         <Link to="/" className="mr-4">Home</Link>
-        <Link to="/admin">Admin</Link>
          <ThemeToggle />
       </div>
     </nav>
